baa9d476c88fb539283b0a62671b5d59e6968a5b
Evgenii Mazrukho <mazrukho@gmail.com>
2023-12-16 08:12:33 +0300

    add files

---
+++ WorkoutApp/Controller/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  WorkoutApp
+//
+//  Created by Evgenii Mazrukho on 16.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
